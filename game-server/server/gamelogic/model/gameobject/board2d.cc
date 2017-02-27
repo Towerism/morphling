be8a457e38b21ac059b49455086b8cc50fbeb349
Martin Fracker <martin.frackerjr@gmail.com>
2017-02-27 10:22:34 -0600

    Add basic game board logic

--- game-server/server/gamelogic/model/gameobject/board2d.cc
+++ game-server/server/gamelogic/model/gameobject/board2d.cc
@@ -1,1 +1,21 @@
 #include "board2d.h"
+
+namespace Morphling::Gamelogic {
+  Board2D::gamepiece_t Board2D::get(int i, int j) {
+    gamepiece_t gamepiece;
+    if (check_dimensions(i, j)) {
+      gamepiece = gamepiece_t(board[i * width + j]);
+    }
+    return gamepiece;
+  }
+
+  void Board2D::set(int i, int j, Game_object* game_object) {
+    if (check_dimensions(i, j)) {
+      board[i * width + j] = gamepiece_t(game_object);
+    }
+  }
+
+  bool Board2D::check_dimensions(int i, int j) {
+    return i > -1 && j > -1 && i < height && j < width;
+  }
+}
---
+++ game-server/server/gamelogic/model/gameobject/board2d.h
@@ -0,0 +1,28 @@
+#include <memory>
+#include <vector>
+
+#include "game_object.h"
+
+namespace Morphling::Gamelogic {
+  class Board2D : public Game_object {
+  public:
+    typedef std::shared_ptr<Game_object> gamepiece_t;
+    typedef std::vector<gamepiece_t> board_t;
+
+    Board2D(int height, int width) : height(height), width(width), board(height * width) {}
+    virtual ~Board2D();
+
+    gamepiece_t get(int i, int j);
+    void set(int i, int j, Game_object* game_object);
+
+    int get_width() const { return width; }
+    int get_height() const { return height; }
+
+  private:
+    int height, width;
+    board_t board;
+
+    bool check_dimensions(int i, int j);
+  };
+
+}
