b7e8711ab5fb4185d4dada375e7f8f36b503f0e0
Martin Fracker <martin.frackerjr@gmail.com>
2017-04-24 12:30:00 -0500

    Map tokens on a per-controller basis instead of per-engine

--- game-server/server/gamelogic/controller/controller.h
+++ game-server/server/gamelogic/controller/controller.h
@@ -6,6 +6,7 @@
 #include <gamelogic/model/model.h>
 #include <gamelogic/model/player.h>
 #include <gamelogic/view/model_to_strings_mapper.h>
+#include <gamelogic/view/token_to_url_mapper.h>
 #include "action.h"
 
 namespace Morphling::Gamelogic {
@@ -33,10 +34,16 @@ namespace Morphling::Gamelogic {
 
     Model_to_strings_mapper::strings_t serialize_model() { return model_serializer->get_strings(); }
     void set_model_serializer(Model_to_strings_mapper* mapper) { model_serializer = mapper; }
+    std::string map_token_to_url(char token) const { return token_to_url_mapper.get_url(token); }
+    void add_url(char token, std::string url) { token_to_url_mapper.add_url(token, url); }
+
 
   protected:
     virtual void execute_action(action_t action) = 0;
     Model* model;
+
+  private:
+    Token_to_url_mapper token_to_url_mapper;
     Model_to_strings_mapper* model_serializer;
   };
 
