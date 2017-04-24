369ce1aa06b09372afe6a69ec663cae345027c54
Siyu Lin <siyul@tamu.edu>
2017-04-23 22:41:08 -0500

    General config for ranking and settings.

---
+++ web-server/static/js/firebase_config.js
@@ -0,0 +1,8 @@
+var config = {
+  apiKey: "AIzaSyAA5mXv5xQiWS1kbnkz8gV6mgNPb3ttVf0",
+  authDomain: "flask-experiment-e7196.firebaseapp.com",
+  databaseURL: "https://flask-experiment-e7196.firebaseio.com",
+  storageBucket: "flask-experiment-e7196.appspot.com",
+  messagingSenderId: "771513661920"
+};
+firebase.initializeApp(config);
